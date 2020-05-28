88b0d9e0077210e3b2cf0261e5cf778b651a8439
chinapandaman <benjaminli801@gmail.com>
2020-05-28 22:07:42 +0000

    sqlite poc

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
+const Sequelize = require("sequelize");
+
 var express = require('express');
 var app = express();
+
+const db = new Sequelize({
+    dialect: "sqlite",
+    storage: "db.sqlite",
+});
+
+db.authenticate()
+    .then(() => {console.log("connected");})
+    .catch(err => {console.error(err);});
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
