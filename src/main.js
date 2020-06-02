56884d8e7d01eef98d2640d9d902231e2f540f2d
chinapandaman <benjaminli801@gmail.com>
2020-06-02 13:57:57 +0000

    prettier

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-var express = require('express');
+var express = require("express");
 var app = express();
 
 const db = new Sequelize({
@@ -9,13 +9,17 @@ const db = new Sequelize({
 });
 
 db.authenticate()
-    .then(() => {console.log("connected");})
-    .catch(err => {console.error(err);})
+    .then(() => {
+        console.log("connected");
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.get("/", function (req, res) {
+    res.send("Hello World!");
 });
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log("Example app listening on port 3000!");
 });
